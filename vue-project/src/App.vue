7e062a9bc41ba3f1061cbdf42d8d27c7ef2cc688
Caitlyn Caldwell <cecldwll@gmail.com>
2026-03-14 18:14:43 -0600

    components

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,23 +1,7 @@
 <script setup>
-import { ref } from 'vue'
-
-const todoId = ref(1)
-const todoData = ref(null)
-
-async function fetchData() {
-  todoData.value = null
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
-  )
-  todoData.value = await res.json()
-}
-
-fetchData()
+import ChildComp from './ChildComp.vue'
 </script>
 
 <template>
-  <p>Todo id: {{ todoId }}</p>
-  <button @click="todoId++" :disabled="!todoData">Fetch next todo</button>
-  <p v-if="!todoData">Loading...</p>
-  <pre v-else>{{ todoData }}</pre>
+  <ChildComp />
 </template>
